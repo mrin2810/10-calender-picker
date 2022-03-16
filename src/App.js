1c724f2164f8ca7f84a4229e10b6de926f6bdc6a
mrin2810 <mrinmayeerane2810@gmail.com>
2022-03-16 23:30:23 +0530

    replace components with styled components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
 const calendarDates = Array(31)
@@ -36,34 +37,78 @@ export default function App() {
 
   return (
     <>
-      <div className="date-chooser">
-        <button 
-          className="date-chooser-button"
+      <StyledDateChooser>
+        <StyledDateChooserButton
           onClick={() => setDateType('start')}
         >
           Start Date <span>{startDate}</span>
-        </button>
-        <button 
-          className="date-chooser-button"
+        </StyledDateChooserButton>
+        <StyledDateChooserButton
           onClick={() => setDateType('end')}
         >
           End Date <span>{endDate}</span>
-        </button>
-      </div>
+        </StyledDateChooserButton>
+      </StyledDateChooser>
 
-      <div className="calendar">
+      <StyledCalender>
         {calendarDates.map((day, index) => {
           const dayNum = day + 1;
           let isSelected = (dayNum === startDate || dayNum === endDate) ? 'is-selected' : '';
           let isInRange = (endDate && dayNum > startDate && dayNum < endDate) ? 'is-in-range' : '';
-          return <div 
-            className={`calendar-day ${isSelected} ${isInRange}`}
+          return <StyledCalenderDay
             onClick={() => updateDate(dayNum)}
           >
             {dayNum}
-          </div>;
+          </StyledCalenderDay>;
         })}
-      </div>
+      </StyledCalender>
     </>
   );
 }
+
+const StyledDateChooser = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const StyledDateChooserButton = styled.button`
+  color: #0b204c;
+  text-transform: uppercase;
+  flex: 1;
+  padding: 15px;
+  background: none;
+  cursor: pointer;
+  border: none;
+  border-bottom: 2px solid rgba(11, 32, 76, 0.2);
+  outline: none;
+  span {
+    display: block;
+    min-height: 60px;
+    font-size: 50px;
+  }
+`;
+
+const StyledCalender = styled.div`
+  max-width: 400px;
+  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+    0 10px 10px -5px rgba(0, 0, 0, 0.04);
+  background: #0b204c;
+  color: #fff;
+  padding: 20px;
+`;
+
+const StyledCalenderDay = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  transition: 0.3s ease background;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  color: #8096c1;
+  background: none;
+`;
